062fa5515d5b76bd550cb372e56771f2b95f907d
Amine Grassi <grassi.amine@hotmail.fr>
2021-11-01 23:57:06 +0100

    dix pipe search problem

--- src/app/views/tables/datatable/datafilterpipe.ts
+++ src/app/views/tables/datatable/datafilterpipe.ts
@@ -5,11 +5,28 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'dataFilter'
 })
 export class DataFilterPipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    if (!value) return null;
+    if (!args) return value;
 
-  transform(array: any[], query: string): any {
-    if (query) {
-      return _.filter(array, row => row.name.indexOf(query) > -1);
-    }
-    return array;
+    args = args.toLowerCase();
+
+    //La méthode JSON.stringify() convertit une valeur JavaScript en chaîne JSON.
+    return value.filter((item: any) => {
+      return item.ram?.capacite?.toLowerCase().includes(args) ||
+      item.systeme?.systeme?.toLowerCase().includes(args) ||
+      item.type?.type?.toLowerCase().includes(args) ||
+      item.SerialNumber?.toLowerCase().includes(args) ||
+      item.service?.nomService?.toLowerCase().includes(args)||
+      item.owner?.firstName?.toLowerCase().includes(args)||
+      item.owner?.lastName?.toLowerCase().includes(args)||
+      item.domaine?.toLowerCase().includes(args)||
+      item.application?.application?.toLowerCase().includes(args)||
+      item.situation?.toLowerCase().includes(args)||
+      item.Marque?.marque?.toLowerCase().includes(args);
+
+
+
+    });
   }
 }
